d6f8e4b7dc777c981823846fb56258cadbeca879
Manu <manu@snapdragon.cc>
2018-10-28 01:54:56 +0800

    Small refactor.

--- vorta/__main__.py
+++ vorta/__main__.py
@@ -2,11 +2,12 @@ import sys
 import os
 from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction
 from PyQt5.QtGui import QIcon
-from vorta.main import MainWindow
+from vorta.main_window import MainWindow
 
 app = QApplication(sys.argv)
-app.setQuitOnLastWindowClosed(False)
+
 # Create the tray
+app.setQuitOnLastWindowClosed(False)
 tray = QSystemTrayIcon()
 icon = QIcon(os.path.join(os.path.dirname(__file__), 'UI/icons/hdd-o.png'))
 tray.setIcon(icon)
@@ -16,8 +17,6 @@ tray.setVisible(True)
 menu = QMenu()
 action = QAction("A menu item")
 menu.addAction(action)
-
-# Add the menu to the tray
 tray.setContextMenu(menu)
 
 ex = MainWindow()
