b26db07fee72a9004f3b37695d521dcd2e9f6e5a
Manu <manu@snapdragon.cc>
2018-11-03 00:33:37 +0800

    Add current version to Sentry log.

--- src/vorta/__main__.py
+++ src/vorta/__main__.py
@@ -5,13 +5,16 @@ import peewee
 import vorta.models
 from vorta.application import VortaApp
 from vorta.config import SETTINGS_DIR
+from vorta._version import __version__
 
 
 def main():
     # Send crashes to Sentry
-    if not os.environ.get('NO_SENTRY'):
+    if not os.environ.get('NO_SENTRY', False):
         import sentry_sdk
-        sentry_sdk.init("https://a4a23df3e44743d5b5c5f06417a9a809@sentry.io/1311799")
+        sentry_sdk.init("https://a4a23df3e44743d5b5c5f06417a9a809@sentry.io/1311799",
+                        release=__version__)
+
 
     # Init database
     sqlite_db = peewee.SqliteDatabase(os.path.join(SETTINGS_DIR, 'settings.db'))
