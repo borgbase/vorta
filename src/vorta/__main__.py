a72f84f17d0593460998b1e77fd9efdfe54862ca
Manu <manu@snapdragon.cc>
2018-11-22 00:36:35 +0800

    Add client-side scrubbing of passwords. Fixes #34

--- src/vorta/__main__.py
+++ src/vorta/__main__.py
@@ -6,15 +6,12 @@ import vorta.models
 from vorta.application import VortaApp
 from vorta.config import SETTINGS_DIR
 import vorta.updater
-from vorta._version import __version__
 
 
 def main():
-    # Send crashes to Sentry
+    # Send crashes to Sentry.
     if not os.environ.get('NO_SENTRY', False):
-        import sentry_sdk
-        sentry_sdk.init("https://a4a23df3e44743d5b5c5f06417a9a809@sentry.io/1311799",
-                        release=__version__)
+        import vorta.sentry
 
     # Init database
     sqlite_db = peewee.SqliteDatabase(os.path.join(SETTINGS_DIR, 'settings.db'))
