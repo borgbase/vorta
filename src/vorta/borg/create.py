514d196ae4144632fd5d6509b62eae1838e2295b
Manu <manu@snapdragon.cc>
2018-11-27 19:02:49 +0800

    Fix wifi rule.

--- src/vorta/borg/create.py
+++ src/vorta/borg/create.py
@@ -67,11 +67,12 @@ class BorgCreateThread(BorgThread):
                 (
                     WifiSettingModel.ssid == current_wifi
                 ) & (
-                    WifiSettingModel.allowed is False
+                    WifiSettingModel.allowed == False  # noqa
                 ) & (
-                    WifiSettingModel.profile == profile.id
+                    WifiSettingModel.profile == profile
                 )
             )
+            print(wifi_is_disallowed.count())
             if wifi_is_disallowed.count() > 0 and profile.repo.is_remote_repo():
                 ret['message'] = 'Current Wifi is not allowed.'
                 return ret
