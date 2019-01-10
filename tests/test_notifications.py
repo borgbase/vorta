84f63357317bafbf44d42f824950ee9108210177
Manu <manu@snapdragon.cc>
2019-01-10 19:41:03 +0800

    Attempt to fix Linux tests.

--- tests/test_notifications.py
+++ tests/test_notifications.py
@@ -8,12 +8,14 @@ import vorta.notifications
 
 @pytest.mark.skipif(sys.platform != 'linux', reason="notify2 only on linux")
 def test_linux_background_notifications(app, qtbot, mocker, borg_json_output):
-    mocker.spy(vorta.notifications.LinuxNotifications, 'deliver')
+    import notify2
+
+    mocker.spy(notify2.Notification, 'show')
 
     notifier = vorta.notifications.VortaNotifications.pick()
-    result_err = notifier.deliver('Vorta Test', 'test notification', level='error')
-    result_info = notifier.deliver('Vorta Test', 'test notification', level='info')
+    assert isinstance(notifier, vorta.notifications.LinuxNotifications)
+    notifier.deliver('Vorta Test', 'test notification', level='error')
+    notifier.deliver('Vorta Test', 'test notification', level='info')
 
-    assert vorta.notifications.LinuxNotifications.deliver.call_count == 2
-    assert result_err is True
-    assert result_info is False
+    assert notify2.Notification.show.call_count == 2
+
