10c824dbeb68b50bcf578d3a52ef839b44c2e48f
Thomas Waldmann <tw@waldmann-edv.de>
2019-01-22 02:50:01 +0100

    implement TRANS_SCALE env var for i18n UI layout checking

--- src/vorta/i18n/__init__.py
+++ src/vorta/i18n/__init__.py
@@ -8,11 +8,45 @@ from PyQt5.QtCore import QTranslator, QLocale
 
 logger = logging.getLogger(__name__)
 
+# To check if the UI layout copes flexibly with translated strings, an env var
+# TRANS_SCALE can be used - it will result in transformed translated strings, e.g.:
+# 100 = scale 100%, normal
+# N>0 = scale to N%
+# N<0 = additionally, do a RTL simulation at scale -N%
+# usage: TRANS_SCALE=200 LANG=de_DE vorta --foreground
+trans_scale = int(os.environ.get('TRANS_SCALE', '100'))
+
+
+class VortaTranslator(QTranslator):
+    def translate(self, context, text, disambiguation=None, n=-1):
+        translated = super().translate(context, text, disambiguation=disambiguation, n=n)
+        scale = trans_scale
+        if scale == 100:  # normal, production usage
+            return translated
+
+        # for UI layout debugging:
+        has_placeholders = '%' in translated
+        has_html = translated.startswith('<') and translated.endswith('>')
+        if has_placeholders or has_html:
+            # not supported kinds of strings, just return normal:
+            return translated
+        if scale < 0:
+            # for simple RTL checking: reverse translated string
+            translated = translated[::-1]
+            scale = -scale
+        if 0 < scale < 100:
+            step = 100 // scale
+            scale = None
+        else:
+            step = None
+            scale = scale // 100
+        return translated * scale if step is None else translated[::step]
+
 
 def init_translations(app):
     global application, translator, locale  # if we don't keep a reference on these, it stops working. pyqt bug?
     application = app
-    translator = QTranslator()
+    translator = VortaTranslator()
     locale = QLocale()
     qm_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'qm'))
     ui_langs = locale.uiLanguages()
