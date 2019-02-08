eff99a6bb7b97022a0515bccfb46847970f632e3
Thomas Waldmann <tw@waldmann-edv.de>
2019-02-08 22:24:29 +0100

    compressor selection: add zlib, add zstd,8, descriptions, fixes #145

--- src/vorta/views/repo_tab.py
+++ src/vorta/views/repo_tab.py
@@ -31,9 +31,20 @@ class RepoTab(RepoBase, RepoUI, BackupProfileMixin):
         self.repoSelector.currentIndexChanged.connect(self.repo_select_action)
         self.repoRemoveToolbutton.clicked.connect(self.repo_unlink_action)
 
-        self.repoCompression.addItem(self.tr('LZ4 (default)'), 'lz4')
-        self.repoCompression.addItem(self.tr('Zstandard (medium)'), 'zstd')
-        self.repoCompression.addItem(self.tr('LZMA (high)'), 'lzma,6')
+        # note: it is hard to describe these algorithms with attributes like low/medium/high
+        # compression or speed on a unified scale. this is not 1-dimensional and also depends
+        # on the input data. so we just tell what we know for sure.
+        # "auto" is used for some slower / older algorithms to avoid wasting a lot of time
+        # on uncompressible data.
+        self.repoCompression.addItem(self.tr('LZ4 [modern, default]'), 'lz4')
+        self.repoCompression.addItem(self.tr('Zstandard Level 3 [modern]'), 'zstd,3')
+        self.repoCompression.addItem(self.tr('Zstandard Level 8 [modern]'), 'zstd,8')
+        # zlib and lzma come from python stdlib and are there (and in borg) since long.
+        # but maybe not much reason to start with these nowadays, considering zstd supports
+        # a very wide range of compression levels and has great speed. if speed is more
+        # important than compression, lz4 is even a little better.
+        self.repoCompression.addItem(self.tr('ZLIB Level 6 [auto, legacy]'), 'auto,zlib,6')
+        self.repoCompression.addItem(self.tr('LZMA Level 6 [auto, legacy]'), 'auto,lzma,6')
         self.repoCompression.addItem(self.tr('No Compression'), 'none')
         self.repoCompression.currentIndexChanged.connect(self.compression_select_action)
 
