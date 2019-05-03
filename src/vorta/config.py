0f156145f08495b390c94043fc27dd416ae76f66
Julian Hofer <julian.public@protonmail.com>
2019-05-03 20:57:05 +0200

    Make Flatpak Ready 1) Change socket-file path so that Flatpaks has access 2) Change autostart function so that it detects if it is run as Flatpak

--- src/vorta/config.py
+++ src/vorta/config.py
@@ -6,9 +6,13 @@ APP_AUTHOR = 'BorgBase'
 dirs = appdirs.AppDirs(APP_NAME, APP_AUTHOR)
 SETTINGS_DIR = dirs.user_data_dir
 LOG_DIR = dirs.user_log_dir
+STATE_DIR = dirs.user_state_dir
 
 if not os.path.exists(SETTINGS_DIR):
     os.makedirs(SETTINGS_DIR)
 
 if not os.path.exists(LOG_DIR):
     os.makedirs(LOG_DIR)
+
+if not os.path.exists(STATE_DIR):
+    os.makedirs(STATE_DIR)
