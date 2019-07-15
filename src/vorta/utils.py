85c9f97cee1c9fd0152ccda1d9f35123e4e038f8
Julian Hofer <julian.public@protonmail.com>
2019-07-15 09:45:00 +0200

    Rename background-option to daemonize

--- src/vorta/utils.py
+++ src/vorta/utils.py
@@ -182,9 +182,9 @@ def parse_args():
                         action='store_true',
                         help="Don't fork into background and open main window on startup.")
     if sys.platform.startswith("linux"):
-        parser.add_argument('--background', '-b',
+        parser.add_argument('--daemonize', '-d',
                             action='store_true',
-                            help="Fork into background. This option takes precedence over other settings.")
+                            help="Daemonize and don't open window on startup.")
 
     return parser.parse_known_args()[0]
 
