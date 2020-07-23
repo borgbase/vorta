a2706f8b1bdc60ed5c9907f2cd13a7102eb8d6e9
samuel-w <samuel.woon@protonmail.com>
2020-07-23 00:55:45 -0500

    Store passwords statically

--- src/vorta/keyring/ram.py
+++ src/vorta/keyring/ram.py
@@ -6,15 +6,12 @@ class VortaMemoryKeyring(VortaKeyring):
     Our own alternative fallback keyring service. Stores the
     passwords in RAM using a dictionary, and is lost on exit.
     """
-
-    def __init__(self):
-        self.passwords = {}
-
+    passwords = {}
     def set_password(self, service, repo_url, password):
-        self.passwords[repo_url] = password
+        VortaMemoryKeyring.passwords[repo_url] = password
 
     def get_password(self, service, repo_url):
-        return self.passwords.get(repo_url)
+        return VortaMemoryKeyring.passwords.get(repo_url)
 
     @property
     def is_primary(self):
