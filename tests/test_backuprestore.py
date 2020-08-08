80ecdd5a489a652f85334115f89dd74da4d65c29
samuel-w <samuel.woon@protonmail.com>
2020-08-07 19:35:23 -0500

    Improve tests

--- tests/test_backuprestore.py
+++ tests/test_backuprestore.py
@@ -1,24 +1,46 @@
 from PyQt5 import QtCore
+import PyQt5.QtWidgets
 import os
 
 from vorta.views.backup_window import RestoreWindow
 from vorta.models import BackupProfileModel
 
 
-def test_restore_success(qapp, qtbot, rootdir):
+def test_restore_success(qapp, qtbot, rootdir, monkeypatch):
+    GOOD_FILE = os.path.join(rootdir, "testcase.vortabackup")
+
+    def getOpenFileName(*args, **kwargs):
+        return [GOOD_FILE]
+
+    monkeypatch.setattr(
+        PyQt5.QtWidgets.QFileDialog, "getOpenFileName", getOpenFileName
+    )
+
     main = qapp.main_window
     restore_dialog = RestoreWindow(BackupProfileModel.get(id=main.profileSelector.currentData()))
-    restore_dialog.saveButton.setEnabled(True)
-    restore_dialog.locationLabel.setText(os.path.join(rootdir, "testcase.vortabackup"))
+
+    qtbot.mouseClick(restore_dialog.fileButton, QtCore.Qt.LeftButton)
+    qtbot.waitUntil(lambda: restore_dialog.locationLabel.text() == GOOD_FILE, timeout=5000)
+
     qtbot.mouseClick(restore_dialog.saveButton, QtCore.Qt.LeftButton)
-    assert(restore_dialog.errors.text() == "")
+    qtbot.waitUntil(lambda: restore_dialog.errors.text() == "", timeout=5000)
 
 
-def test_restore_fail(qapp, qtbot, rootdir):
+def test_restore_fail(qapp, qtbot, rootdir, monkeypatch):
+    BAD_FILE = os.path.join(rootdir, "invalid.vortabackup")
+
+    def getOpenFileName(*args, **kwargs):
+        return [BAD_FILE]
+
+    monkeypatch.setattr(
+        PyQt5.QtWidgets.QFileDialog, "getOpenFileName", getOpenFileName
+    )
     main = qapp.main_window
     restore_dialog = RestoreWindow(BackupProfileModel.get(id=main.profileSelector.currentData()))
-    restore_dialog.saveButton.setEnabled(True)
-    restore_dialog.locationLabel.setText(os.path.join(rootdir, "invalid.vortabackup"))
+
+    qtbot.mouseClick(restore_dialog.fileButton, QtCore.Qt.LeftButton)
+    qtbot.waitUntil(lambda: restore_dialog.locationLabel.text() == BAD_FILE, timeout=5000)
+
     qtbot.mouseClick(restore_dialog.saveButton, QtCore.Qt.LeftButton)
-    assert(restore_dialog.errors.text() == "Invalid backup file")
+    qtbot.waitUntil(lambda: restore_dialog.errors.text() == "Invalid backup file", timeout=5000)
     qtbot.mouseClick(restore_dialog.cancelButton, QtCore.Qt.LeftButton)
