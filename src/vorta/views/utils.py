856fb73f14744f9ea2824e6db33943fb05ebfb5c
Samuel Woon <samuel.woon@protonmail.com>
2020-09-18 19:59:42 -0500

    Set all icons to same height (#660)

--- src/vorta/views/utils.py
+++ src/vorta/views/utils.py
@@ -9,9 +9,7 @@ def get_colored_icon(icon_name):
     svg_str = open(get_asset(f"icons/{icon_name}.svg"), 'rb').read()
     if uses_dark_mode():
         svg_str = svg_str.replace(b'#00000', b'#ffffff')
-    svg_img = QImage.fromData(svg_str)
-
-    # Reduce image size to 1/14th
-    svg_img = svg_img.scaled(svg_img.size() / 14)
+    # Reduce image size to 128 height
+    svg_img = QImage.fromData(svg_str).scaledToHeight(128)
 
     return QIcon(QPixmap(svg_img))
