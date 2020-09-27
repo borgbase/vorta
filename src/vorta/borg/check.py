a00300e7b9c03680da97f214bf8bb7ad00cf9576
samuel-w <samuel.woon@protonmail.com>
2020-09-26 20:37:14 -0500

    Update check message on completion

--- src/vorta/borg/check.py
+++ src/vorta/borg/check.py
@@ -10,6 +10,7 @@ class BorgCheckThread(BorgThread):
     def finished_event(self, result):
         self.app.backup_finished_event.emit(result)
         self.result.emit(result)
+        self.app.backup_progress_event.emit(self.tr('Check completed.'))
 
     @classmethod
     def prepare(cls, profile):
