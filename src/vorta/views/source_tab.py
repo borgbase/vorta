f67203ee51d5a2b6f6f41f3ecd8b845229fbad07
XXXBold <30773753+XXXBold@users.noreply.github.com>
2020-09-27 17:52:07 +0000

    run lint

--- src/vorta/views/source_tab.py
+++ src/vorta/views/source_tab.py
@@ -1,5 +1,5 @@
 from PyQt5 import uic
-from ..models import SourceFileModel, BackupProfileMixin, SettingsModel
+from ..models import SourceFileModel, BackupProfileMixin
 from ..utils import get_asset, choose_file_dialog, pretty_bytes, FilePathInfoAsync
 from PyQt5 import QtCore
 from PyQt5.QtCore import QFileInfo
@@ -12,19 +12,19 @@ SourceUI, SourceBase = uic.loadUiType(uifile)
 
 class SourceTab(SourceBase, SourceUI, BackupProfileMixin):
     updateThreads = []
-    
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(parent)
-        
-        headerTxt=["Path","Type","Size","Elements Count"]
+
+        headerTxt = ["Path", "Type", "Size", "Elements Count"]
 
         self.sourceFilesWidget.setColumnCount(len(headerTxt))
         header = self.sourceFilesWidget.horizontalHeader()
 
         header.setVisible(True)
         header.setSortIndicatorShown(1)
-        
+
         header.setSectionResizeMode(0, QHeaderView.Stretch)
         header.setSectionResizeMode(1, QHeaderView.ResizeToContents)
         header.setSectionResizeMode(2, QHeaderView.ResizeToContents)
@@ -35,54 +35,44 @@ class SourceTab(SourceBase, SourceUI, BackupProfileMixin):
         self.sourceAddFolder.clicked.connect(lambda: self.source_add(want_folder=True))
         self.sourceAddFile.clicked.connect(lambda: self.source_add(want_folder=False))
         self.sourceRemove.clicked.connect(self.source_remove)
-        self.sourcesUpdate.clicked.connect(self.sources_update)
         self.paste.clicked.connect(self.paste_text)
         self.excludePatternsField.textChanged.connect(self.save_exclude_patterns)
         self.excludeIfPresentField.textChanged.connect(self.save_exclude_if_present)
         self.populate_from_profile()
-        
-    def set_path_info(self,path,data_size,files_count):
-        items = self.sourceFilesWidget.findItems(path,QtCore.Qt.MatchExactly)
+
+    def set_path_info(self, path, data_size, files_count):
+        items = self.sourceFilesWidget.findItems(path, QtCore.Qt.MatchExactly)
         for item in items:
-            self.sourceFilesWidget.item(item.row(),2).setText(pretty_bytes(data_size))
-            self.sourceFilesWidget.item(item.row(),3).setText(format(files_count))
+            self.sourceFilesWidget.item(item.row(), 2).setText(pretty_bytes(data_size))
+            self.sourceFilesWidget.item(item.row(), 3).setText(format(files_count))
             db_item = SourceFileModel.get(dir=path)
             db_item.dir_size = data_size
             db_item.dir_files_count = files_count
             db_item.save()
-        # Remove thread from list when it's done
-        for thrd in self.updateThreads:
-            if thrd.objectName() == path:
-                self.updateThreads.remove(thrd)
 
-    def update_path_info(self,index_row):
-        path = self.sourceFilesWidget.item(index_row,0).text()
-        self.sourceFilesWidget.setItem(index_row,2,QTableWidgetItem("load..."))
-        self.sourceFilesWidget.setItem(index_row,3,QTableWidgetItem("load..."))
-        getDir = FilePathInfoAsync(path)
-        getDir.signal.connect(self.set_path_info)
-        getDir.setObjectName(path)
-        self.updateThreads.append(getDir) # this is ugly, is there a better way to keep the thread object?
-        getDir.start()
-
-    def add_source_to_table(self,source,update_data):
-        index_row = self.sourceFilesWidget.rowCount()
-        self.sourceFilesWidget.insertRow(index_row)
-        item_path = QTableWidgetItem(source.dir)
-        self.sourceFilesWidget.setItem(index_row,0,item_path)
+    def add_source_to_table(self, source, update_data):
+        indexRow = self.sourceFilesWidget.rowCount()
+        self.sourceFilesWidget.insertRow(indexRow)
+        itemPath = QTableWidgetItem(source.dir)
+        self.sourceFilesWidget.setItem(indexRow, 0, itemPath)
         if source.path_isdir == True:
-            self.sourceFilesWidget.setItem(index_row,1,QTableWidgetItem("Folder"))
+            self.sourceFilesWidget.setItem(indexRow, 1, QTableWidgetItem("<DIR>"))
         else:
-            self.sourceFilesWidget.setItem(index_row,1,QTableWidgetItem("File"))
+            self.sourceFilesWidget.setItem(indexRow, 1, QTableWidgetItem("<File>"))
         if update_data == True:
-            self.update_path_info(index_row)
-        else: # Use cached data from DB
+            self.sourceFilesWidget.setItem(indexRow, 2, QTableWidgetItem("load..."))
+            self.sourceFilesWidget.setItem(indexRow, 3, QTableWidgetItem("load..."))
+            getDir = FilePathInfoAsync(source.dir)
+            getDir.signal.connect(self.set_path_info)
+            self.updateThreads.append(getDir)  # this is ugly, is there a better way to keep the thread object?
+            getDir.start()
+        else:  # Use cached data from DB
             if source.dir_size < 0:
-                self.sourceFilesWidget.setItem(index_row,2,QTableWidgetItem("N/A"))
-                self.sourceFilesWidget.setItem(index_row,3,QTableWidgetItem("N/A"))
+                self.sourceFilesWidget.setItem(indexRow, 2, QTableWidgetItem("N/A"))
+                self.sourceFilesWidget.setItem(indexRow, 3, QTableWidgetItem("N/A"))
             else:
-                self.sourceFilesWidget.setItem(index_row,2,QTableWidgetItem(pretty_bytes(source.dir_size)))
-                self.sourceFilesWidget.setItem(index_row,3,QTableWidgetItem(format(source.dir_files_count)))
+                self.sourceFilesWidget.setItem(indexRow, 2, QTableWidgetItem(pretty_bytes(source.dir_size)))
+                self.sourceFilesWidget.setItem(indexRow, 3, QTableWidgetItem(format(source.dir_files_count)))
 
     def populate_from_profile(self):
         profile = self.profile()
@@ -93,28 +83,24 @@ class SourceTab(SourceBase, SourceUI, BackupProfileMixin):
         self.excludeIfPresentField.clear()
 
         for source in SourceFileModel.select().where(SourceFileModel.profile == profile):
-            self.add_source_to_table(source,False)
+            self.add_source_to_table(source, False)
 
         self.excludePatternsField.appendPlainText(profile.exclude_patterns)
         self.excludeIfPresentField.appendPlainText(profile.exclude_if_present)
         self.excludePatternsField.textChanged.connect(self.save_exclude_patterns)
         self.excludeIfPresentField.textChanged.connect(self.save_exclude_if_present)
 
-    def sources_update(self):
-        for row in range(0, self.sourceFilesWidget.rowCount()):
-            self.update_path_info(row) # Update data for each entry
-
     def source_add(self, want_folder):
         def receive():
             dirs = dialog.selectedFiles()
             for dir in dirs:
-                new_source, created = SourceFileModel.get_or_create(dir=dir, 
-                                                                    dir_size=-1, 
-                                                                    dir_files_count=-1, 
-                                                                    path_isdir=QFileInfo(dir).isDir(), 
+                new_source, created = SourceFileModel.get_or_create(dir=dir,
+                                                                    dir_size=-1,
+                                                                    dir_files_count=-1,
+                                                                    path_isdir=QFileInfo(dir).isDir(),
                                                                     profile=self.profile())
                 if created:
-                    self.add_source_to_table(new_source,SettingsModel.get(key="get_srcpath_datasize").value)
+                    self.add_source_to_table(new_source, True)
                     new_source.save()
 
         msg = self.tr("Choose directory to back up") if want_folder else self.tr("Choose file(s) to back up")
@@ -127,7 +113,7 @@ class SourceTab(SourceBase, SourceUI, BackupProfileMixin):
         indexes.sort()
         # remove each selected row, starting with highest index (otherways, higher indexes become invalid)
         for index in reversed(indexes):
-            db_item = SourceFileModel.get(dir=self.sourceFilesWidget.item(index.row(),0).text())
+            db_item = SourceFileModel.get(dir=self.sourceFilesWidget.item(index.row(), 0).text())
             db_item.delete_instance()
             self.sourceFilesWidget.removeRow(index.row())
 
