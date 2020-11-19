4ad1dca7633acfd3394e88a6667c2c079a081881
samuel-w <samuel.woon@protonmail.com>
2020-11-19 15:15:13 -0600

    Adjust profile test timeout

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -10,7 +10,7 @@ def test_profile_add(qapp, qtbot):
 
     add_profile_window = main.window
     qtbot.addWidget(add_profile_window)
-    qtbot.waitUntil(lambda: add_profile_window == qapp.activeWindow())
+    qtbot.waitUntil(lambda: add_profile_window == qapp.activeWindow(), timeout=5000)
 
     qtbot.keyClicks(add_profile_window.profileNameField, 'Test Profile')
     qtbot.mouseClick(add_profile_window.buttonBox.button(QDialogButtonBox.Save), QtCore.Qt.LeftButton)
@@ -27,7 +27,7 @@ def test_profile_edit(qapp, qtbot):
 
     edit_profile_window = main.window
     qtbot.addWidget(edit_profile_window)
-    qtbot.waitUntil(lambda: edit_profile_window == qapp.activeWindow())
+    qtbot.waitUntil(lambda: edit_profile_window == qapp.activeWindow(), timeout=5000)
 
     edit_profile_window.profileNameField.setText("")
     qtbot.keyClicks(edit_profile_window.profileNameField, 'Test Profile')
