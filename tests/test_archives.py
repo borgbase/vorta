601facb0fe7079b18108e28b813608c7919025c0
Samuel <samuel.woon@protonmail.com>
2020-11-30 19:32:14 -0600

    Disable mount test on macOS
    https://github.com/borgbackup/borg/issues/5522

--- tests/test_archives.py
+++ tests/test_archives.py
@@ -78,6 +78,7 @@ def test_check(qapp, mocker, borg_json_output, qtbot):
     qtbot.waitUntil(lambda: main.logText.text().startswith(success_text), timeout=3000)
 
 
+@pytest.mark.skipif(sys.platform == 'darwin', reason="Test currently broken by Homebrew")
 def test_archive_mount(qapp, qtbot, mocker, borg_json_output, monkeypatch, choose_file_dialog):
     def psutil_disk_partitions(**kwargs):
         DiskPartitions = namedtuple('DiskPartitions', ['device', 'mountpoint'])
