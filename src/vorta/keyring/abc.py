41e015bf8f9b8a4d6dd2adb608f72eb3c458cd9e
samuel-w <samuel.woon@protonmail.com>
2020-12-03 21:24:36 -0600

    Lint

--- src/vorta/keyring/abc.py
+++ src/vorta/keyring/abc.py
@@ -51,13 +51,13 @@ def get_keyring():
         else:  # Try to use DBus and Gnome-Keyring (available on Linux and *BSD)
             import secretstorage
             from .secretstorage import VortaSecretStorageKeyring
-            
+
             # secretstorage has two different libraries based on version
             if parse_version(secretstorage.__version__) >= parse_version("3.0.0"):
                 from jeepney.wrappers import DBusErrorResponse as DBusException
             else:
                 from dbus.exceptions import DBusException
-                
+
             try:
                 _keyring = VortaSecretStorageKeyring()
             except (secretstorage.exceptions.SecretStorageException, DBusException):  # Try to use KWallet (KDE)
