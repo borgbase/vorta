1c8ecbc9c7797d9ce4633f457087e9ef9e359849
samuel-w <samuel.woon@protonmail.com>
2021-01-06 17:59:13 -0600

    Simplify setting filler password

--- src/vorta/borg/borg_thread.py
+++ src/vorta/borg/borg_thread.py
@@ -66,11 +66,7 @@ class BorgThread(QtCore.QThread, BackupProfileMixin):
         if 'additional_env' in params:
             env = {**env, **params['additional_env']}
 
-        password = params.get('password')
-        if password is not None:
-            env['BORG_PASSPHRASE'] = password
-        else:
-            env['BORG_PASSPHRASE'] = '9999999'  # Set dummy password to avoid prompt.
+        env['BORG_PASSPHRASE'] = params.get('password', '9999999')  # Set dummy password to avoid prompt.
 
         if env.get('BORG_PASSCOMMAND', False):
             env.pop('BORG_PASSPHRASE', None)  # Unset passphrase
