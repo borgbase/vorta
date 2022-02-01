103a3a7aaf8ae1497aa8c8b33dd651ee5de773cb
real-yfprojects <real-yfprojects@users.noreply.github.com>
2022-02-01 17:00:10 +0100

    Group settings widgets in `misctab`.
    * src/vorta/assets/UI/misctab.ui (checkboxLayout): Replace with frame `frameSettings` containing a form layout.
    
    * src/vorta/utils.py (search): Added. Searches for a key inside an iterable
    	applying a given function before comparison.
    
    * src/vorta/store/models.py (SettingsModel): Add field `group` to assign settings to a group.
    
    * src/vorta/store/migrations.py (run_migrations): Add new schema version `19` and
    	implement migration adding the `group` field.
    
    * src/vorta/store/connection.py (SCHEMA_VERSION): Update to `19`.
    
    * src/vorta/store/settings.py (get_misc_settings): Add and assign settings groups.
    
    * src/vorta/store/connection.py (init_db): Update group and type of settings if needed.
    
    * src/vorta/views/misc_tab.py (Imports): Import `search` from `..utils`.
    
    * src/vorta/views/misc_tab.py : Instanciate logger.
    
    * src/vorta/views/misc_tab.py (MiscTab.__init__): Create a checkboxLayout for `frameSettings`.
    
    * src/vorta/views/misc_tab.py (populate): Add settings widgets in groups with labels in spacer in between.

--- src/vorta/views/misc_tab.py
+++ src/vorta/views/misc_tab.py
@@ -1,44 +1,97 @@
+import logging
+
 from PyQt5 import uic
-from PyQt5.QtWidgets import QCheckBox
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QCheckBox, QFormLayout, QLabel, QSizePolicy,
+                             QSpacerItem)
 
 from vorta._version import __version__
 from vorta.config import LOG_DIR
 from vorta.i18n import translate
+from vorta.store.models import BackupProfileMixin, SettingsModel
 from vorta.store.settings import get_misc_settings
-from vorta.store.models import SettingsModel, BackupProfileMixin
-from vorta.utils import get_asset
+from vorta.utils import get_asset, search
 
 uifile = get_asset('UI/misctab.ui')
 MiscTabUI, MiscTabBase = uic.loadUiType(uifile)
+
+logger = logging.getLogger(__name__)
 
 
 class MiscTab(MiscTabBase, MiscTabUI, BackupProfileMixin):
 
     def __init__(self, parent=None):
+        """Init."""
         super().__init__(parent)
         self.setupUi(parent)
         self.versionLabel.setText(__version__)
         self.logLink.setText(f'<a href="file://{LOG_DIR}"><span style="text-decoration:'
                              'underline; color:#0984e3;">Log</span></a>')
 
+        self.checkboxLayout = QFormLayout(self.frameSettings)
+        self.checkboxLayout.setSpacing(4)
+        self.checkboxLayout.setHorizontalSpacing(8)
+        self.checkboxLayout.setContentsMargins(0, 0, 0, 12)
+        self.checkboxLayout.setFormAlignment(Qt.AlignmentFlag.AlignHCenter)
+
         self.populate()
 
     def populate(self):
+        """
+        Populate the misc tab with the settings widgets.
+
+        Uses `create_group_widget` to construct the layout groups.
+        """
         # clear layout
         while self.checkboxLayout.count():
             child = self.checkboxLayout.takeAt(0)
+            self.checkboxLayout.removeItem(child)
             if child.widget():
                 child.widget().deleteLater()
+
         # dynamically add widgets for settings
-        for setting in SettingsModel.select().where(SettingsModel.type == 'checkbox'):
-            x = filter(lambda s: s['key'] == setting.key, get_misc_settings())
-            if not list(x):  # Skip settings that aren't specified in vorta.store.models.
-                continue
-            b = QCheckBox(translate('settings', setting.label))
-            b.setCheckState(setting.value)
-            b.setTristate(False)
-            b.stateChanged.connect(lambda v, key=setting.key: self.save_setting(key, v))
-            self.checkboxLayout.addWidget(b)
+        misc_settings = get_misc_settings()
+
+        i = 0
+        for group in (SettingsModel.select(SettingsModel.group)
+                      .distinct(True).where(SettingsModel.group != '')
+                      .order_by(SettingsModel.group.asc())):
+            # add spacer
+            if i > 0:
+                spacer = QSpacerItem(20, 4, vPolicy=QSizePolicy.Policy.Fixed)
+                self.checkboxLayout.setItem(i,
+                                            QFormLayout.ItemRole.LabelRole,
+                                            spacer)
+                i += 1
+
+            # add label for next group
+            label = QLabel()
+            label.setText(translate('settings', group.group) + ':')
+            self.checkboxLayout.setWidget(i, QFormLayout.ItemRole.LabelRole, label)
+
+            # add settings widget of the group
+            for setting in (SettingsModel.select()
+                            .where(SettingsModel.type == 'checkbox',
+                                   SettingsModel.group == group.group)):
+                # Skip settings that aren't specified in vorta.store.models.
+                if not search(setting.key, misc_settings, lambda d: d['key']):
+                    logger.warning('Unknown setting {}'.format(setting.key))
+                    continue
+
+                # create widget
+                cb = QCheckBox(translate('settings', setting.label))
+                cb.setCheckState(setting.value)
+                cb.setTristate(False)
+                cb.stateChanged.connect(
+                    lambda v, key=setting.key: self.save_setting(key, v))
+
+                # add widget
+                self.checkboxLayout.setWidget(i,
+                                              QFormLayout.ItemRole.FieldRole,
+                                              cb)
+
+                # increase i
+                i += 1
 
     def save_setting(self, key, new_value):
         setting = SettingsModel.get(key=key)
