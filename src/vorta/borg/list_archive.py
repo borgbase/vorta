borgbase/vorta

commit 0188b753b492cd6aee65fa8efd87ae5f48c61a99

Use `isomtime` instead of `mtime` from borg v1.2.2 on.
* src/vorta/borg/list_archive.py (BorgListArchiveJob.prepare)

* src/vorta/views/extract_dialog.py (parse_json_lines)

src/vorta/borg/list_archive.py
@@ -1,3 +1,4 @@
+from vorta.utils import borg_compat
 from .borg_job import BorgJob
 
 
@@ -26,8 +27,9 @@ class BorgListArchiveJob(BorgJob):
             '--json-lines',
             '--format',
             # fields to include in json output
-            "{mode}{user}{group}{size}{mtime}{path}{source}{health}{NL}",
-            f'{profile.repo.url}::{archive_name}',
+            "{mode}{user}{group}{size}{" + 'isomtime'
+            if borg_compat.check('V122')
+            else 'mtime' + "}{path}{source}{health}{NL}",
         ]
         ret['ok'] = True
 

src/vorta/views/extract_dialog.py
@@ -9,7 +9,7 @@ from PyQt5 import uic
 from PyQt5.QtCore import QDateTime, QLocale, QMimeData, QModelIndex, QPoint, Qt, QThread, QUrl
 from PyQt5.QtGui import QColor, QKeySequence
 from PyQt5.QtWidgets import QApplication, QDialogButtonBox, QHeaderView, QMenu, QPushButton, QShortcut
-from vorta.utils import get_asset, pretty_bytes, uses_dark_mode
+from vorta.utils import borg_compat, get_asset, pretty_bytes, uses_dark_mode
 from vorta.views.utils import get_colored_icon
 from .partials.treemodel import FileSystemItem, FileTreeModel, FileTreeSortProxyModel, path_to_str, relative_path
 
@@ -208,7 +208,9 @@ def parse_json_lines(lines, model: "ExtractTree"):
         # except ValueError:
         #     modified = datetime.strptime(item["mtime"], "%Y-%m-%dT%H:%M:%S")
 
-        modified = QDateTime.fromString(item["mtime"], Qt.DateFormat.ISODateWithMs)
+        modified = QDateTime.fromString(
+            item['isomtime' if borg_compat.check('V122') else 'mtime'], Qt.DateFormat.ISODateWithMs
+        )
 
         model.addItem(
             (

commit 9cff2d280f53225aefb1237165978c4c613b2377

Supply repository and archive name to borg in `BorgListArchiveJob`.
This reverts wrong modifications introduced in 0188b753b492cd6aee65fa8efd87ae5f48c61a99.

* src/vorta/borg/list_archive.py (BorgListArchiveJob.prepare)

src/vorta/borg/list_archive.py
@@ -30,6 +30,7 @@ class BorgListArchiveJob(BorgJob):
             "{mode}{user}{group}{size}{"
             + ('isomtime' if borg_compat.check('V122') else 'mtime')
             + "}{path}{source}{health}{NL}",
+            f'{profile.repo.url}::{archive_name}',
         ]
         ret['ok'] = True
 