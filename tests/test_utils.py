3ee099d64c87224fb1d401b9da05bcfb6ff57a53
Manu <3916435+m3nu@users.noreply.github.com>
2022-12-16 11:19:34 +0100

    Use new ssh://-style URL format as placeholder and in test (#1473)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,7 @@ from vorta.keyring.abc import VortaKeyring
 
 def test_keyring():
     UNICODE_PW = 'kjalsdfüadsfäadsfß'
-    REPO = f'vorta-test-repo.{uuid.uuid4()}.com:repo'  # Random repo URL
+    REPO = f'ssh://asdf123@vorta-test-repo.{uuid.uuid4()}.com/./repo'  # Random repo URL
 
     keyring = VortaKeyring.get_keyring()
     keyring.set_password('vorta-repo', REPO, UNICODE_PW)
