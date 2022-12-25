410f0d7dde7d349010590bc16436e2738f2097bf
endumiuz <endumiuz@gmail.com>
2022-12-25 13:26:58 +0100

    clean up
    Co-authored-by: yfprojects <62463991+real-yfprojects@users.noreply.github.com>

--- src/vorta/borg/borg_job.py
+++ src/vorta/borg/borg_job.py
@@ -277,8 +277,7 @@ class BorgJob(JobInterface, BackupProfileMixin):
 
                         elif parsed['type'] == 'file_status':
                             self.app.backup_log_event.emit(f'{parsed["path"]} ({parsed["status"]})', {})
-                        elif parsed['type'] == 'progress_percent':
-                            if parsed.get("message"):
+                        elif parsed['type'] == 'progress_percent' and parsed.get("message"):
                                 self.app.backup_log_event.emit(f'{parsed["message"]}', {})
                         elif parsed['type'] == 'archive_progress' and not parsed.get('finished', False):
                             msg = (
