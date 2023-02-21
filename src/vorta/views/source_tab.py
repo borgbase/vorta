50d30b7505de774e13353e6821b350de95e0dfec
Roberto Previdi <hariseldon78@gmail.com>
2023-02-21 22:01:49 +0100

    Improve size column readability in archives tab (#1598)
    Use one size unit for all archives. The unit is selected by a simple algorithm that picks the largest unit that can represent the smallest size with a given precision. Align all sizes in archive and source tab to the left.
    
    * src/vorta/utils.py : Implement `find_best_size_formatting`. Add missing sizes to `sort_sizes`. Simplify `pretty_bytes` and add `fixed_unit` option for use with `find_best_size_formatting`. Implement `clamp` utility function. Add type hints and docstrings.
    
    * src/vorta/views/archive_tab.py (ArchiveTab.populate_from_profile): Use `find_best_sizes_formatting`.
    
    * src/vorta/views/source_tab.py (SizeItem.__init__): Set alignment to left.
    
    * tests/test_utils.py : Add comprehensive tests for `pretty_bytes` and `find_best_sizes_formatting`.
    
    Co-authored-by: yfprojects <62463991+real-yfprojects@users.noreply.github.com>

--- src/vorta/views/source_tab.py
+++ src/vorta/views/source_tab.py
@@ -21,6 +21,10 @@ class SourceColumn:
 
 
 class SizeItem(QTableWidgetItem):
+    def __init__(self, s):
+        super().__init__(s)
+        self.setTextAlignment(Qt.AlignVCenter + Qt.AlignRight)
+
     def __lt__(self, other):
         if other.text() == '':
             return False
