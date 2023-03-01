19896bcd645677e9d7e986b013c5eb2d9a44f0dd
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-03-01 17:45:28 +0530

    Refactoring using pathlibs.Paths

--- src/vorta/config.py
+++ src/vorta/config.py
@@ -6,9 +6,9 @@ APP_NAME = 'Vorta'
 APP_AUTHOR = 'BorgBase'
 APP_ID_DARWIN = 'com.borgbase.client.macos'
 dirs = platformdirs.PlatformDirs(APP_NAME, APP_AUTHOR)
-SETTINGS_DIR = dirs.user_data_dir
-LOG_DIR = dirs.user_log_dir
-CACHE_DIR = dirs.user_cache_dir
+SETTINGS_DIR = dirs.user_data_path
+LOG_DIR = dirs.user_log_path
+CACHE_DIR = dirs.user_cache_path
 TEMP_DIR = os.path.join(CACHE_DIR, "tmp")
 PROFILE_BOOTSTRAP_FILE = Path.home() / '.vorta-init.json'
 
