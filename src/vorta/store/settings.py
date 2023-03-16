28773efad0a8fbbc52885033993356610ae4b5f0
bigtedde <big.tedde@gmail.com>
2023-03-15 20:01:48 -0700

    created setting for full disk access check at startup

--- src/vorta/store/settings.py
+++ src/vorta/store/settings.py
@@ -83,6 +83,17 @@ def get_misc_settings() -> List[Dict[str, str]]:
             'tooltip': trans_late('settings', 'Set owner to current user and umask to 0277'),
         },
         {
+            'key': 'check_full_disk_access',
+            'value': True,
+            'type': 'checkbox',
+            'group': startup,
+            'label': trans_late(
+                'settings',
+                'Check for Full Disk Access on startup',
+            ),
+            'tooltip': trans_late('settings', 'Alerts user when full disk access permission has not been provided'),
+        },
+        {
             'key': 'previous_profile_id',
             'str_value': '1',
             'type': 'internal',
