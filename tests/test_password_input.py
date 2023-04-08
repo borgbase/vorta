4a271800e8a9bab2c89eaf3a3a2b86f3415f6f62
Chirag Aggarwal <thechiragaggarwal@gmail.com>
2023-04-08 16:49:05 -0400

    New Tests for PasswordLineEdit
    Signed-off-by: Chirag Aggarwal <thechiragaggarwal@gmail.com>

---
+++ tests/test_password_input.py
@@ -0,0 +1,61 @@
+from vorta.views.partials.password_input import PasswordInput, PasswordLineEdit
+
+
+def test_create_password_line_edit(qtbot):
+    password_line_edit = PasswordLineEdit()
+    qtbot.addWidget(password_line_edit)
+    assert password_line_edit is not None
+
+
+def test_password_line_get_password(qtbot):
+    password_line_edit = PasswordLineEdit()
+    qtbot.addWidget(password_line_edit)
+
+    password_line_edit.setText("test")
+    assert password_line_edit.get_password() == "test"
+
+
+def test_password_line_visible(qtbot):
+    password_line_edit = PasswordLineEdit()
+    qtbot.addWidget(password_line_edit)
+    assert password_line_edit.visible is False
+
+    password_line_edit.toggle_visibility()
+    assert password_line_edit.visible
+
+
+def test_password_line_error_state(qtbot):
+    password_line_edit = PasswordLineEdit()
+    qtbot.addWidget(password_line_edit)
+    assert password_line_edit.error_state is False
+
+    password_line_edit.error_state = True
+    assert password_line_edit.error_state
+    assert password_line_edit.styleSheet() == "border: 2px solid red;"
+
+
+def test_password_lint_visibility_button(qtbot):
+    password_line_edit = PasswordLineEdit(show_visibility_button=False)
+    qtbot.addWidget(password_line_edit)
+    assert password_line_edit._show_visibility_button is False
+
+    password_line_edit = PasswordLineEdit()
+    qtbot.addWidget(password_line_edit)
+    assert password_line_edit._show_visibility_button
+
+    # test visibility button
+    password_line_edit.showHideAction.trigger()
+    assert password_line_edit.visible
+    password_line_edit.showHideAction.trigger()
+    assert password_line_edit.visible is False
+
+
+# PasswordInput
+def test_create_password_input(qtbot):
+    password_input = PasswordInput()
+    qtbot.addWidget(password_input.create_form_widget())
+    assert password_input is not None
+
+    # test default error state
+    assert password_input.passwordLineEdit.error_state is False
+    assert password_input.confirmLineEdit.error_state is False
