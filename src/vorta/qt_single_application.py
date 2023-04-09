4a496de83f0e616f9700cb1ca8a4b8cf3f056427
Manu <manu@snapdragon.cc>
2023-04-09 15:47:43 +0100

    Remove encoding setting. UTF8 already default.

--- src/vorta/qt_single_application.py
+++ src/vorta/qt_single_application.py
@@ -52,7 +52,6 @@ class QtSingleApplication(QApplication):
         if self._isRunning:
             # Yes, there is.
             self._outStream = QTextStream(self._outSocket)
-            self._outStream.setCodec('UTF-8')
         else:
             # No, there isn't.
             self._outSocket = None
@@ -84,7 +83,6 @@ class QtSingleApplication(QApplication):
         if not self._inSocket:
             return
         self._inStream = QTextStream(self._inSocket)
-        self._inStream.setCodec('UTF-8')
         self._inSocket.readyRead.connect(self._onReadyRead)
 
     def _onReadyRead(self):
