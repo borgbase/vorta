b851baee8e25895a121b38cb0c23849852f16887
real-yfprojects <real-yfprojects@users.noreply.github.com>
2023-04-16 12:53:22 +0200

    Fix qt enum comparison in `vorta.borg.extract`.
    * src/vorta/borg/extract.py (BorgExtractJob.prepare): Compare `item.data.checkstate` to type `Qt.Checkstate` instead of the value of the enum

--- src/vorta/borg/extract.py
+++ src/vorta/borg/extract.py
@@ -51,7 +51,7 @@ class BorgExtractJob(BorgJob):
                 indexes.append(new_index)
 
                 item: FileSystemItem[FileData] = new_index.internalPointer()
-                if item.data.checkstate == Qt.CheckState.Checked.value:
+                if item.data.checkstate == Qt.CheckState.Checked:
                     pattern_file.write("+ " + path_to_str(item.path) + "\n")
 
         pattern_file.write("- fm:*\n")
