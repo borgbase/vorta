23ccbde4ff176ce9cb32f1b6524793b3f256c3a2
Chirag Aggarwal <thechiragaggarwal@gmail.com>
2023-05-20 15:48:35 -0400

    Initial nox configuration file
    Signed-off-by: Chirag Aggarwal <thechiragaggarwal@gmail.com>

---
+++ noxfile.py
@@ -0,0 +1,26 @@
+import re
+import nox
+
+@nox.session
+@nox.parametrize("borgbackup", ["2.0.0b5", "2.0.0b4"])
+def run_tests(session, borgbackup):
+    # install borgbackup
+    session.install(f"borgbackup=={borgbackup}")
+
+    # install dependencies
+    session.install("-r", "requirements.d/dev.txt")
+    session.install("-e", ".")
+
+    # check versions
+    cli_version = session.run("borg", "--version", silent=True).strip()
+    cli_version = re.search(r"borg (\S+)", cli_version).group(1)
+    python_version = session.run("python", "-c", "import borg; print(borg.__version__)", silent=True).strip()
+
+    session.log(f"CLI version: {cli_version}")
+    session.log(f"Python version: {python_version}")
+    
+    assert cli_version == borgbackup
+    assert python_version == borgbackup
+
+    # run tests
+    session.run("pytest")
