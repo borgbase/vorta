e456f1009a2f37fd1a5c515b6477d9a027747c5b
Chirag Aggarwal <thechiragaggarwal@gmail.com>
2023-06-09 01:21:39 -0400

    Test for creating new archive
    Signed-off-by: Chirag Aggarwal <thechiragaggarwal@gmail.com>

---
+++ tests/integration/test_repo.py
@@ -0,0 +1,20 @@
+import pytest
+from PyQt6 import QtCore
+from vorta.store.models import ArchiveModel, EventLogModel
+
+
+def test_create(qapp, qtbot):
+    main = qapp.main_window
+    main.show()
+    main.archiveTab.refresh_archive_list()
+    qtbot.waitUntil(lambda: main.archiveTab.archiveTable.rowCount() > 0, **pytest._wait_defaults)
+
+    qtbot.mouseClick(main.createStartBtn, QtCore.Qt.MouseButton.LeftButton)
+    qtbot.waitUntil(lambda: 'Backup finished.' in main.progressText.text(), **pytest._wait_defaults)
+    qtbot.waitUntil(lambda: main.createStartBtn.isEnabled(), **pytest._wait_defaults)
+
+    assert EventLogModel.select().count() == 2
+    assert ArchiveModel.select().count() == 7
+    assert main.createStartBtn.isEnabled()
+    assert main.archiveTab.archiveTable.rowCount() == 7
+    assert main.scheduleTab.logTableWidget.rowCount() == 2
