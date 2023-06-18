db7a0a872fd77265ab6d6ed7c94db02fc64cb945
Divyansh Singh <divyanshanandsingh@gmail.com>
2023-06-19 02:30:55 +0530

    use icons instead of text

--- src/vorta/views/archive_tab.py
+++ src/vorta/views/archive_tab.py
@@ -14,6 +14,7 @@ from PyQt6.QtWidgets import (
     QLayout,
     QMenu,
     QMessageBox,
+    QStyledItemDelegate,
     QTableView,
     QTableWidgetItem,
     QWidget,
@@ -57,6 +58,13 @@ logger = logging.getLogger(__name__)
 SIZE_DECIMAL_DIGITS = 1
 
 
+# from https://stackoverflow.com/questions/63177587/pyqt-tableview-align-icons-to-center
+class IconDelegate(QStyledItemDelegate):
+    def initStyleOption(self, option, index):
+        super().initStyleOption(option, index)
+        option.decorationSize = option.rect.size() - QtCore.QSize(0, 10)
+
+
 class ArchiveTab(ArchiveTabBase, ArchiveTabUI, BackupProfileMixin):
     prune_intervals = ['hour', 'day', 'week', 'month', 'year']
 
@@ -83,7 +91,10 @@ class ArchiveTab(ArchiveTabBase, ArchiveTabUI, BackupProfileMixin):
         header.setSectionResizeMode(2, QHeaderView.ResizeMode.ResizeToContents)
         header.setSectionResizeMode(3, QHeaderView.ResizeMode.Interactive)
         header.setSectionResizeMode(4, QHeaderView.ResizeMode.Stretch)
-        header.setStretchLastSection(True)
+        header.setSectionResizeMode(5, QHeaderView.ResizeMode.ResizeToContents)
+
+        delegate = IconDelegate(self.archiveTable)
+        self.archiveTable.setItemDelegateForColumn(5, delegate)
 
         if sys.platform != 'darwin':
             self._set_status('')  # Set platform-specific hints.
@@ -280,8 +291,15 @@ class ArchiveTab(ArchiveTabBase, ArchiveTabUI, BackupProfileMixin):
 
                 self.archiveTable.setItem(row, 4, QTableWidgetItem(archive.name))
 
-                if archive.trigger:
-                    self.archiveTable.setItem(row, 5, QTableWidgetItem(archive.trigger.capitalize()))
+                if archive.trigger == 'scheduled':
+                    item = QTableWidgetItem(get_colored_icon('clock-o'), '')
+                    item.setToolTip(self.tr('Scheduled'))
+                    self.archiveTable.setItem(row, 5, item)
+                elif archive.trigger == 'user':
+                    item = QTableWidgetItem(get_colored_icon('user'), '')
+                    item.setToolTip(self.tr('User initiated'))
+                    item.setTextAlignment(Qt.AlignmentFlag.AlignRight)
+                    self.archiveTable.setItem(row, 5, item)
 
             self.archiveTable.setRowCount(len(archives))
             self.archiveTable.setSortingEnabled(sorting)
