6e1c70c62ca0e4879d1478850da4f6440c80c40b
Chirag Aggarwal <thechiragaggarwal@gmail.com>
2023-06-24 13:17:24 -0400

    Nox config to use env specified borg version when set
    Signed-off-by: Chirag Aggarwal <thechiragaggarwal@gmail.com>

--- noxfile.py
+++ noxfile.py
@@ -1,31 +1,40 @@
+import os
 import platform
 import re
+import sys
 
 import nox
 
-system_python_version = platform.python_version_tuple()
-system_python_version = tuple(int(part) for part in system_python_version)
+borg_version = os.getenv("BORG_VERSION")
 
-supported_borgbackup_versions = [
-    borgbackup
-    for borgbackup in ("1.1.18", "1.2.2", "1.2.4", "2.0.0b5")
-    # Python version requirements for borgbackup versions
-    if (borgbackup == "1.1.18" and system_python_version >= (3, 5, 0))
-    or (borgbackup == "1.2.2" and system_python_version >= (3, 8, 0))
-    or (borgbackup == "1.2.4" and system_python_version >= (3, 8, 0))
-    or (borgbackup == "2.0.0b5" and system_python_version >= (3, 9, 0))
-]
+if borg_version:
+    # Use specified borg version
+    supported_borgbackup_versions = [borg_version]
+else:
+    # Generate a list of borg versions compatible with system installed python version
+    system_python_version = platform.python_version_tuple()
+    system_python_version = tuple(int(part) for part in system_python_version)
+
+    supported_borgbackup_versions = [
+        borgbackup
+        for borgbackup in ("1.1.18", "1.2.2", "1.2.4", "2.0.0b5")
+        # Python version requirements for borgbackup versions
+        if (borgbackup == "1.1.18" and system_python_version >= (3, 5, 0))
+        or (borgbackup == "1.2.2" and system_python_version >= (3, 8, 0))
+        or (borgbackup == "1.2.4" and system_python_version >= (3, 8, 0))
+        or (borgbackup == "2.0.0b5" and system_python_version >= (3, 9, 0))
+    ]
 
 
 @nox.session
-# @nox.parametrize("borgbackup", supported_borgbackup_versions)
+@nox.parametrize("borgbackup", supported_borgbackup_versions)
 def run_tests(session, borgbackup):
 
-    # # install borgbackup
-    # if (borgbackup == "1.1.18"):
-    #     session.install(f"borgbackup=={borgbackup}")
-    # else:
-    #     session.install(f"borgbackup[pyfuse3]=={borgbackup}")
+    # install borgbackup
+    if (borgbackup == "1.1.18" or sys.platform == 'darwin'):
+        session.install(f"borgbackup=={borgbackup}")
+    else:
+        session.install(f"borgbackup[pyfuse3]=={borgbackup}")
 
     # install dependencies
     session.install("-r", "requirements.d/dev.txt")
