62d8e108ce5d64eaef9498f63bf584b3322d5666
Divyansh Singh <divyanshanandsingh@gmail.com>
2023-07-19 21:16:21 +0530

    pass the exclusions to borg!

--- src/vorta/borg/create.py
+++ src/vorta/borg/create.py
@@ -165,23 +165,11 @@ class BorgCreateJob(BorgJob):
         # Add excludes
         # Partly inspired by borgmatic/borgmatic/borg/create.py
         if profile.exclude_patterns is not None:
-            exclude_dirs = []
-            for p in profile.exclude_patterns.split('\n'):
-                if p.strip():
-                    expanded_directory = os.path.expanduser(p.strip())
-                    exclude_dirs.append(expanded_directory)
-
-            if exclude_dirs:
-                pattern_file = tempfile.NamedTemporaryFile('w', delete=True)
-                pattern_file.write('\n'.join(exclude_dirs))
-                pattern_file.flush()
-                cmd.extend(['--exclude-from', pattern_file.name])
-                ret['cleanup_files'].append(pattern_file)
-
-        if profile.exclude_if_present is not None:
-            for f in profile.exclude_if_present.split('\n'):
-                if f.strip():
-                    cmd.extend(['--exclude-if-present', f.strip()])
+            pattern_file = tempfile.NamedTemporaryFile('w', delete=True)
+            pattern_file.write(profile.exclude_patterns)
+            pattern_file.flush()
+            cmd.extend(['--exclude-from', pattern_file.name])
+            ret['cleanup_files'].append(pattern_file)
 
         # Add repo url and source dirs.
         new_archive_name = format_archive_name(profile, profile.new_archive_name)
