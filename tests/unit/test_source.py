eb489f36d3c9cb726e648b34a9cd1b73ae528ff6
bigtedde <big.tedde@gmail.com>
2023-08-09 16:54:54 -0700

    added 'test_sources_update'

--- tests/unit/test_source.py
+++ tests/unit/test_source.py
@@ -3,15 +3,33 @@ import vorta.views
 from PyQt6.QtWidgets import QMessageBox
 
 
-def test_source_add_remove(qapp, qtbot, monkeypatch, choose_file_dialog):
-    monkeypatch.setattr(vorta.views.source_tab, "choose_file_dialog", choose_file_dialog)
-    main = qapp.main_window
-    main.tabWidget.setCurrentIndex(1)
-    tab = main.sourceTab
+@pytest.fixture()
+def sources_setup(qapp, qtbot, monkeypatch, choose_file_dialog):
+    def setup():
+        monkeypatch.setattr(vorta.views.source_tab, "choose_file_dialog", choose_file_dialog)
+        main = qapp.main_window
+        main.tabWidget.setCurrentIndex(1)
+        tab = main.sourceTab
+        qtbot.waitUntil(lambda: tab.sourceFilesWidget.rowCount() == 1, **pytest._wait_defaults)
+        return main, tab
 
-    # test adding a folder
+    return setup
+
+
+def test_source_add_remove(qapp, qtbot, monkeypatch, mocker, sources_setup):
+    main, tab = sources_setup()
+
+    # test adding a folder with os access
+    mocker.patch('os.access', return_value=True)
     tab.source_add(want_folder=True)
     qtbot.waitUntil(lambda: tab.sourceFilesWidget.rowCount() == 2, **pytest._wait_defaults)
+    assert tab.sourceFilesWidget.rowCount() == 2
+
+    # test adding a folder without os access
+    mocker.patch('os.access', return_value=False)
+    monkeypatch.setattr(QMessageBox, "exec", lambda *args: True)  # prevent QMessageBox from stopping test
+    tab.source_add(want_folder=True)
+    assert tab.sourceFilesWidget.rowCount() == 2
 
     # test removing a folder
     tab.sourceFilesWidget.selectRow(1)
@@ -30,22 +48,42 @@ def test_source_add_remove(qapp, qtbot, monkeypatch, choose_file_dialog):
         (f"file://{__file__}{__file__}", False),  # invalid - no new line separating file names
     ],
 )
-def test_paste_text(qapp, qtbot, mocker, monkeypatch, choose_file_dialog, path, valid):
-    monkeypatch.setattr(vorta.views.source_tab, "choose_file_dialog", choose_file_dialog)
-    main = qapp.main_window
-    main.tabWidget.setCurrentIndex(1)
-    tab = main.sourceTab
-
+def test_paste_text(qapp, qtbot, mocker, monkeypatch, sources_setup, path, valid):
+    main, tab = sources_setup()
     mock_clipboard = mocker.Mock()
     mock_clipboard.text.return_value = path
+
     mocker.patch.object(vorta.views.source_tab.QApplication, 'clipboard', return_value=mock_clipboard)
-    monkeypatch.setattr(QMessageBox, "exec", lambda *args: True)
+    monkeypatch.setattr(QMessageBox, "exec", lambda *args: True)  # prevent QMessageBox from stopping test
     tab.paste_text()
+
     if valid:
+        # valid paths will be added as a source
         assert not hasattr(tab, '_msg')
         qtbot.waitUntil(lambda: tab.sourceFilesWidget.rowCount() == 2, **pytest._wait_defaults)
+        assert tab.sourceFilesWidget.rowCount() == 2
         # Wait for directory sizing to finish
         qtbot.waitUntil(lambda: len(qapp.main_window.sourceTab.updateThreads) == 0, **pytest._wait_defaults)
     else:
+        # invalid paths will trigger an alert and not be added as a source
         qtbot.waitUntil(lambda: hasattr(tab, "_msg"), **pytest._wait_defaults)
         assert tab._msg.text().startswith("Some of your sources are invalid")
+        assert tab.sourceFilesWidget.rowCount() == 1
+
+
+def test_sources_update(qapp, qtbot, mocker, sources_setup):
+    main, tab = sources_setup()
+
+    # test that `update_path_info()` has been called for each source path
+    update_path_info_spy = mocker.spy(tab, "update_path_info")
+    tab.sources_update()
+    assert tab.sourceFilesWidget.rowCount() == 1
+    assert update_path_info_spy.call_count == 1
+
+    # add a new source and test again
+    tab.source_add(want_folder=True)
+    qtbot.waitUntil(lambda: tab.sourceFilesWidget.rowCount() == 2, **pytest._wait_defaults)
+    update_path_info_spy.reset_mock()
+    tab.sources_update()
+    assert tab.sourceFilesWidget.rowCount() == 2
+    assert update_path_info_spy.call_count == 2
