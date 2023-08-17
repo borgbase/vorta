8c3e54413e56d7cd3493f37cabda5f4e3cf40d51
bigtedde <big.tedde@gmail.com>
2023-08-16 17:08:58 -0700

    updated fixture, included docstrings

--- tests/unit/test_source.py
+++ tests/unit/test_source.py
@@ -6,15 +6,15 @@ from PyQt6.QtWidgets import QMessageBox
 
 @pytest.fixture()
 def source_env(qapp, qtbot, monkeypatch, choose_file_dialog):
-    def setup():
-        monkeypatch.setattr(vorta.views.source_tab, "choose_file_dialog", choose_file_dialog)
-        main = qapp.main_window
-        main.tabWidget.setCurrentIndex(1)
-        tab = main.sourceTab
-        qtbot.waitUntil(lambda: tab.sourceFilesWidget.rowCount() == 1, timeout=2000)
-        return main, tab
+    """
+    Handles common setup and teardown for unit tests involving the source tab.
+    """
+    monkeypatch.setattr(vorta.views.source_tab, "choose_file_dialog", choose_file_dialog)
+    main = qapp.main_window
+    main.tabWidget.setCurrentIndex(1)
+    tab = main.sourceTab
+    qtbot.waitUntil(lambda: tab.sourceFilesWidget.rowCount() == 1, timeout=2000)
 
-    main, tab = setup()
     yield main, tab
 
     # Wait for directory sizing to finish
@@ -22,6 +22,9 @@ def source_env(qapp, qtbot, monkeypatch, choose_file_dialog):
 
 
 def test_source_add_remove(qapp, qtbot, monkeypatch, mocker, source_env):
+    """
+    Tests adding and removing source to ensure expected behavior.
+    """
     main, tab = source_env
     mocker.patch.object(QMessageBox, "exec")  # prevent QMessageBox from stopping test
 
@@ -53,7 +56,11 @@ def test_source_add_remove(qapp, qtbot, monkeypatch, mocker, source_env):
         (f"file://{__file__}{__file__}", False),  # invalid - no new line separating file names
     ],
 )
-def test_paste_text(qapp, qtbot, mocker, source_env, path, valid):
+def test_valid_and_invalid_source_paths(qapp, qtbot, mocker, source_env, path, valid):
+    """
+    Valid paths will be added as a source.
+    Invalid paths will trigger an alert and not be added as a source.
+    """
     main, tab = source_env
     mock_clipboard = mocker.Mock()
     mock_clipboard.text.return_value = path
@@ -63,18 +70,19 @@ def test_paste_text(qapp, qtbot, mocker, source_env, path, valid):
     tab.paste_text()
 
     if valid:
-        # valid paths will be added as a source
         assert not hasattr(tab, '_msg')
         qtbot.waitUntil(lambda: tab.sourceFilesWidget.rowCount() == 2, **pytest._wait_defaults)
         assert tab.sourceFilesWidget.rowCount() == 2
     else:
-        # invalid paths will trigger an alert and not be added as a source
         qtbot.waitUntil(lambda: hasattr(tab, "_msg"), **pytest._wait_defaults)
         assert tab._msg.text().startswith("Some of your sources are invalid")
         assert tab.sourceFilesWidget.rowCount() == 1
 
 
 def test_sources_update(qapp, qtbot, mocker, source_env):
+    """
+    Tests the source update button in the source tab
+    """
     main, tab = source_env
     update_path_info_spy = mocker.spy(tab, "update_path_info")
 
