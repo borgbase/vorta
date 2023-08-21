965563349091dc369c5e94cbc4a3a93608fccef9
bigtedde <big.tedde@gmail.com>
2023-08-20 19:39:35 -0700

    corrected json formatting

--- .github/scripts/generate-matrix.sh
+++ .github/scripts/generate-matrix.sh
@@ -6,25 +6,25 @@ if [[ "$event_name" == "workflow_dispatch" ]] || [[ "$branch_name" == "master" ]
       "python-version": ["3.8", "3.9", "3.10", "3.11"],
       "os": ["ubuntu-latest", "macos-latest"],
       "borg-version": ["1.2.4"]
-    }' > matrix-unit.json
+    }' | jq -c . > matrix-unit.json
 
     echo '{
       "python-version": ["3.8", "3.9", "3.10", "3.11"],
       "os": ["ubuntu-latest", "macos-latest"],
       "borg-version": ["1.1.18", "1.2.2", "1.2.4", "2.0.0b5"],
       "exclude": [{"borg-version": "2.0.0b5", "python-version": "3.8"}]
-    }' > matrix-integration.json
+    }' | jq -c . > matrix-integration.json
 
 elif [[ "$event_name" == "push" ]] || [[ "$event_name" == "pull_request" ]]; then
     echo '{
       "python-version": ["3.8", "3.9", "3.10", "3.11"],
       "os": ["ubuntu-latest", "macos-latest"],
       "borg-version": ["1.2.4"]
-    }' > matrix-unit.json
+    }' | jq -c . > matrix-unit.json
 
     echo '{
       "python-version": ["3.10"],
       "os": ["ubuntu-latest", "macos-latest"],
       "borg-version": ["1.2.4"]
-    }' > matrix-integration.json
+    }' | jq -c . > matrix-integration.json
 fi
