cae7ba4e0f754df3136f74d2eb80f264a33045d6
Divyansh Singh <divyanshanandsingh@gmail.com>
2023-08-25 02:34:05 +0530

    add test for exclude if present dialog

--- tests/test_exclude_if_present.py
+++ tests/test_exclude_if_present.py
@@ -6,21 +6,15 @@ def test_exclusion_preview_populated(qapp, qtbot):
     tab = main.sourceTab
     main.tabWidget.setCurrentIndex(1)
 
-    qtbot.mouseClick(tab.bExclude, QtCore.Qt.MouseButton.LeftButton)
+    qtbot.mouseClick(tab.bExcludeIfPresent, QtCore.Qt.MouseButton.LeftButton)
     qtbot.mouseClick(tab._window.bAddPattern, QtCore.Qt.MouseButton.LeftButton)
 
     qtbot.keyClicks(tab._window.customExclusionsList.viewport().focusWidget(), "custom pattern")
     qtbot.keyClick(tab._window.customExclusionsList.viewport().focusWidget(), QtCore.Qt.Key.Key_Enter)
+
     qtbot.waitUntil(
         lambda: tab._window.exclusionsPreviewText.toPlainText() == "# custom added rules\ncustom pattern\n\n"
     )
-
-    tab._window.tabWidget.setCurrentIndex(1)
-
-    tab._window.exclusionPresetsModel.itemFromIndex(tab._window.exclusionPresetsModel.index(0, 0)).setCheckState(
-        QtCore.Qt.CheckState.Checked
-    )
-    qtbot.waitUntil(lambda: "# Chromium cache and config files" in tab._window.exclusionsPreviewText.toPlainText())
 
     tab._window.tabWidget.setCurrentIndex(2)
 
