fea508631f5323842b643df2f6422f7580d8f9af
bigtedde <big.tedde@gmail.com>
2023-10-19 20:31:29 -0700

    removed test_about - was not adding value.

--- tests/unit/test_about.py
+++
@@ -1,9 +0,0 @@
-from vorta._version import __version__
-
-
-def test_vorta_and_borg_info(qapp):
-    tab = qapp.main_window.aboutTab
-
-    assert tab.vorta_version == __version__
-    assert tab.borg_version == tab.borgVersion.text()
-    assert tab.borg_path in tab.borgPath.text()
