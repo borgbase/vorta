80033250deb62c967a7053d9d96836262d8eb3ca
bigtedde <big.tedde@gmail.com>
2023-10-19 20:23:32 -0700

    fixed aboutTab test

--- tests/unit/test_about.py
+++ tests/unit/test_about.py
@@ -2,7 +2,7 @@ from vorta._version import __version__
 
 
 def test_vorta_and_borg_info(qapp):
-    tab = qapp.main.aboutTab
+    tab = qapp.main_window.aboutTab
 
     assert tab.vorta_version == __version__
     assert tab.borg_version == tab.borgVersion.text()
