baf07a51f109d7a1b2286a3fe4ef2d54b4776ed0
Aryaman <teddybearlaughs@gmail.com>
2024-03-06 19:30:00 +0530

    delete existing sources on profile overwrite #1942

--- src/vorta/profile_export.py
+++ src/vorta/profile_export.py
@@ -130,6 +130,8 @@ class ProfileExport:
         # Set the profile ids to be match new profile
         for source in self._profile_dict['SourceFileModel']:
             source['profile'] = self.id
+        for source in SourceFileModel.select().where(SourceFileModel.profile == source['profile']):
+            SourceFileModel.delete_by_id(source)
         SourceFileModel.insert_many(self._profile_dict['SourceFileModel']).execute()
 
         # Delete added dictionaries to make it match BackupProfileModel
