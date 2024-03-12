6a80518760bd1a9c77d5bc83d4b00608a99f6b8a
Adwait Salankar <salankar.adwait@gmail.com>
2024-03-12 23:59:33 +0530

    Support disabling file logging

--- src/vorta/log.py
+++ src/vorta/log.py
@@ -13,22 +13,21 @@ from vorta import config
 
 logger = logging.getLogger()
 
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+fh = TimedRotatingFileHandler(config.LOG_DIR / 'vorta.log', when='d', interval=1, backupCount=5)
+fh.namer = lambda log_name: log_name.replace(".log", "") + ".log"
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+
 
 def init_logger(background=False):
     logger.setLevel(logging.DEBUG)
     logging.getLogger('peewee').setLevel(logging.INFO)
     logging.getLogger('PyQt6').setLevel(logging.INFO)
 
-    # create logging format
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-    # create handlers
-    fh = TimedRotatingFileHandler(config.LOG_DIR / 'vorta.log', when='d', interval=1, backupCount=5)
-    # ensure ".log" suffix
-    fh.namer = lambda log_name: log_name.replace(".log", "") + ".log"
-    fh.setLevel(logging.DEBUG)
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
+    # Enable file logging by default at the start, since SettingsModel isn't initialised
+    toggle_file_logging(True)
 
     if background:
         pass
@@ -37,3 +36,23 @@ def init_logger(background=False):
         ch.setLevel(logging.DEBUG)
         ch.setFormatter(formatter)
         logger.addHandler(ch)
+
+
+def init_file_logging():
+    """
+    Decides file logging based on the user's preference
+    """
+    from vorta.store.models import SettingsModel
+
+    toggle_file_logging(SettingsModel.get(key='enable_logging_to_file').value)
+
+
+def toggle_file_logging(should_log_to_file):
+    """
+    Enables file logging according to the input
+    """
+
+    if should_log_to_file:
+        logger.addHandler(fh)
+    else:
+        logger.removeHandler(fh)
