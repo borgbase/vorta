c29a9c3f28b41bd47e25bef9551c7f7e2487338e
shivansh02 <ss398@snu.edu.in>
2024-06-12 14:31:23 +0530

    exclude dialog test fixed

--- tests/test_excludes.py
+++ tests/test_excludes.py
@@ -11,16 +11,23 @@ def test_exclusion_preview_populated(qapp, qtbot):
 
     qtbot.keyClicks(tab._window.customExclusionsList.viewport().focusWidget(), "custom pattern")
     qtbot.keyClick(tab._window.customExclusionsList.viewport().focusWidget(), QtCore.Qt.Key.Key_Enter)
-    qtbot.waitUntil(lambda: tab._window.exclusionsPreviewText.toPlainText() == "# custom added rules\ncustom pattern\n")
+    qtbot.waitUntil(lambda: "custom pattern" in tab._window.exclusionsPreviewText.toPlainText())
 
     tab._window.tabWidget.setCurrentIndex(1)
 
     tab._window.exclusionPresetsModel.itemFromIndex(tab._window.exclusionPresetsModel.index(0, 0)).setCheckState(
         QtCore.Qt.CheckState.Checked
     )
-    qtbot.waitUntil(lambda: "# Chromium cache and config files" in tab._window.exclusionsPreviewText.toPlainText())
 
+    qtbot.waitUntil(lambda: "# chromium-cache" in tab._window.exclusionsPreviewText.toPlainText())
     tab._window.tabWidget.setCurrentIndex(2)
 
     qtbot.keyClicks(tab._window.rawExclusionsText, "test raw pattern 1")
     qtbot.waitUntil(lambda: "test raw pattern 1\n" in tab._window.exclusionsPreviewText.toPlainText())
+
+    qtbot.mouseClick(tab.bExclude, QtCore.Qt.MouseButton.LeftButton)
+    qtbot.mouseClick(tab._window.bAddPatternExcludeIfPresent, QtCore.Qt.MouseButton.LeftButton)
+
+    qtbot.keyClicks(tab._window.excludeIfPresentList.viewport().focusWidget(), "exclude_if_present_file")
+    qtbot.keyClick(tab._window.excludeIfPresentList.viewport().focusWidget(), QtCore.Qt.Key.Key_Enter)
+    qtbot.waitUntil(lambda: "exclude_if_present_file" in tab._window.exclusionsPreviewText.toPlainText())
