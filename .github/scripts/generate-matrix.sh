a1a2ddeb0a1251aaf3573c9fc7b81e53857d80f2
Manu <3916435+m3nu@users.noreply.github.com>
2024-10-18 15:17:34 +0100

    Fix macOS tests (#2095)

--- .github/scripts/generate-matrix.sh
+++ .github/scripts/generate-matrix.sh
@@ -3,28 +3,28 @@ branch_name="$2"
 
 if [[ "$event_name" == "workflow_dispatch" ]] || [[ "$branch_name" == "master" ]]; then
     echo '{
-      "python-version": ["3.8", "3.9", "3.10", "3.11"],
-      "os": ["ubuntu-22.04", "macos-12"],
-      "borg-version": ["1.2.4"]
+      "python-version": ["3.9", "3.10", "3.11"],
+      "os": ["ubuntu-22.04", "macos-14"],
+      "borg-version": ["1.4.0"]
     }' | jq -c . > matrix-unit.json
 
     echo '{
-      "python-version": ["3.8", "3.9", "3.10", "3.11"],
-      "os": ["ubuntu-22.04", "macos-12"],
-      "borg-version": ["1.1.18", "1.2.2", "1.2.4", "2.0.0b5"],
-      "exclude": [{"borg-version": "2.0.0b5", "python-version": "3.8"}]
+      "python-version": ["3.9", "3.10", "3.11"],
+      "os": ["ubuntu-22.04", "macos-14"],
+      "borg-version": ["1.1.18", "1.2.8", "1.4.0", "2.0.0b12"],
+      "exclude": [{"borg-version": "2.0.0b12", "python-version": "3.8"}]
     }' | jq -c . > matrix-integration.json
 
 elif [[ "$event_name" == "push" ]] || [[ "$event_name" == "pull_request" ]]; then
     echo '{
-      "python-version": ["3.8", "3.9", "3.10", "3.11"],
-      "os": ["ubuntu-22.04", "macos-12"],
-      "borg-version": ["1.2.4"]
+      "python-version": ["3.9", "3.11"],
+      "os": ["ubuntu-22.04", "macos-14"],
+      "borg-version": ["1.2.8"]
     }' | jq -c . > matrix-unit.json
 
     echo '{
-      "python-version": ["3.10"],
+      "python-version": ["3.11"],
       "os": ["ubuntu-22.04"],
-      "borg-version": ["1.2.4"]
+      "borg-version": ["1.2.8"]
     }' | jq -c . > matrix-integration.json
 fi
