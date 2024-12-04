64a24d86209be7e48a8d6a260ba5afd5a92f9e74
Manu <3916435+m3nu@users.noreply.github.com>
2024-12-04 13:25:12 +0000

    Ensure pre/post-backup cmds are populated (#2153)

--- src/vorta/views/main_window.py
+++ src/vorta/views/main_window.py
@@ -189,6 +189,7 @@ class MainWindow(MainWindowBase, MainWindowUI):
         self.repoTab.populate_from_profile()
         self.sourceTab.populate_from_profile()
         self.scheduleTab.schedulePage.populate_from_profile()
+        self.scheduleTab.shellCommandsPage.populate_from_profile()
         SettingsModel.update({SettingsModel.str_value: self.current_profile.id}).where(
             SettingsModel.key == 'previous_profile_id'
         ).execute()
