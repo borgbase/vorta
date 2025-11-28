48f22114394b4fe8af3c8aba97c9ed0db8d7353b
Manu <manu@snapdragon.cc>
2025-11-28 13:15:34 +0000

    Bump Borg and Python versions for CI

--- .github/scripts/generate-matrix.sh
+++ .github/scripts/generate-matrix.sh
@@ -3,28 +3,28 @@ branch_name="$2"
 
 if [[ "$event_name" == "workflow_dispatch" ]] || [[ "$branch_name" == "master" ]]; then
     echo '{
-      "python-version": ["3.9", "3.10", "3.11", "3.12"],
+      "python-version": ["3.10", "3.12", "3.13"],
       "os": ["ubuntu-24.04", "macos-14"],
-      "borg-version": ["1.4.0"]
+      "borg-version": ["1.4.2"]
     }' | jq -c . > matrix-unit.json
 
     echo '{
-      "python-version": ["3.11"],
+      "python-version": ["3.12"],
       "os": ["ubuntu-24.04"],
-      "borg-version": ["1.1.18", "1.2.8", "1.4.1"],
+      "borg-version": ["1.1.18", "1.2.8", "1.4.2"],
       "exclude": [{"borg-version": "2.0.0b12", "python-version": "3.8"}]
     }' | jq -c . > matrix-integration.json
 
 elif [[ "$event_name" == "push" ]] || [[ "$event_name" == "pull_request" ]]; then
     echo '{
-      "python-version": ["3.9", "3.12"],
+      "python-version": ["3.10", "3.12"],
       "os": ["ubuntu-24.04", "macos-14"],
       "borg-version": ["1.2.8"]
     }' | jq -c . > matrix-unit.json
 
     echo '{
-      "python-version": ["3.11"],
+      "python-version": ["3.12"],
       "os": ["ubuntu-24.04"],
-      "borg-version": ["1.4.0"]
+      "borg-version": ["1.4.2"]
     }' | jq -c . > matrix-integration.json
 fi
