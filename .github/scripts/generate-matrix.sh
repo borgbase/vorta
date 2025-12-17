e2776bc9e283999399027b5a35ad6002b824f62d
Manu <manu@snapdragon.cc>
2025-12-17 12:11:42 +0000

    Bump test versions, fix failing tests

--- .github/scripts/generate-matrix.sh
+++ .github/scripts/generate-matrix.sh
@@ -5,19 +5,19 @@ if [[ "$event_name" == "workflow_dispatch" ]] || [[ "$branch_name" == "master" ]
     echo '{
       "python-version": ["3.10", "3.12", "3.13"],
       "os": ["ubuntu-24.04", "macos-14"],
-      "borg-version": ["1.4.2"]
+      "borg-version": ["1.4.3"]
     }' | jq -c . > matrix-unit.json
 
     echo '{
       "python-version": ["3.12"],
       "os": ["ubuntu-24.04"],
-      "borg-version": ["1.1.18", "1.2.8", "1.4.2"],
+      "borg-version": ["1.2.8", "1.4.2"],
       "exclude": [{"borg-version": "2.0.0b12", "python-version": "3.8"}]
     }' | jq -c . > matrix-integration.json
 
 elif [[ "$event_name" == "push" ]] || [[ "$event_name" == "pull_request" ]]; then
     echo '{
-      "python-version": ["3.10", "3.12"],
+      "python-version": ["3.12"],
       "os": ["ubuntu-24.04", "macos-14"],
       "borg-version": ["1.2.8"]
     }' | jq -c . > matrix-unit.json
