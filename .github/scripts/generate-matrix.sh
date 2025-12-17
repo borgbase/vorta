59c0476aa9d043c31bdb5d9e4d63749e0bd1e879
Manu <manu@snapdragon.cc>
2025-12-17 12:52:12 +0000

    try macos-15

--- .github/scripts/generate-matrix.sh
+++ .github/scripts/generate-matrix.sh
@@ -4,7 +4,7 @@ branch_name="$2"
 if [[ "$event_name" == "workflow_dispatch" ]] || [[ "$branch_name" == "master" ]]; then
     echo '{
       "python-version": ["3.10", "3.12", "3.13"],
-      "os": ["ubuntu-24.04", "macos-14"],
+      "os": ["ubuntu-24.04", "macos-15"],
       "borg-version": ["1.4.3"]
     }' | jq -c . > matrix-unit.json
 
@@ -18,7 +18,7 @@ if [[ "$event_name" == "workflow_dispatch" ]] || [[ "$branch_name" == "master" ]
 elif [[ "$event_name" == "push" ]] || [[ "$event_name" == "pull_request" ]]; then
     echo '{
       "python-version": ["3.12"],
-      "os": ["ubuntu-24.04", "macos-14"],
+      "os": ["ubuntu-24.04", "macos-15"],
       "borg-version": ["1.2.8"]
     }' | jq -c . > matrix-unit.json
 
