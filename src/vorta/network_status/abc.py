824ab6ff165699da7b706dc7c65cf82a4dca95a7
Ryan Zeigler <zeiglerr@gmail.com>
2026-02-09 13:23:15 -0500

    Wait for network before backing up remote repos. By @rzeigler (#2176)
    * Wait on network for remote repos
    
    Don't try and start jobs with remote repos until the network is up.
    This should prevent job failures when, for instance, waking from sleep.
    
    Mac implementation is a stub currently.
    
    Signed-off-by: Ryan Zeigler <zeiglerr@gmail.com>
    
    * Implement missed interface function in NullNetworkStatusMonitor
    
    Signed-off-by: Ryan Zeigler <zeiglerr@gmail.com>
    
    * Reactive profile scheduling based on network status
    
    NetworkStatusMonitor is now a QObject with signal
    network_status_changed.
    The scheduler listens to this signal and handles rescheduling
    similar to the existing 'wake from suspend' logic works.
    
    Signed-off-by: Ryan Zeigler <zeiglerr@gmail.com>
    
    * Request network status when waking from sleep
    
    It seems that sometimes the signal does't arrive.
    
    Signed-off-by: Ryan Zeigler <zeiglerr@gmail.com>
    
    * Address typos and unused imports
    
    * Only use the NM State for connectivity decisions instead of mixing NMConnectivity and NMState
    
    I chose NMState because there is no Connectivity signal and immediately polling for Connectivity on state change seems to return the old value.
    
    * Autoformat fixes
    
    * Avoid logging network is unavailable when nothing to make up.
    
    * Clearify comment verbiage
    
    * Do not block catchup backups for local repos on network state
    
    ---------
    
    Signed-off-by: Ryan Zeigler <zeiglerr@gmail.com>
    Co-authored-by: Manu <3916435+m3nu@users.noreply.github.com>

--- src/vorta/network_status/abc.py
+++ src/vorta/network_status/abc.py
@@ -2,8 +2,10 @@ import sys
 from datetime import datetime
 from typing import List, NamedTuple, Optional
 
+from PyQt6.QtCore import QObject, pyqtSignal
 
-class NetworkStatusMonitor:
+
+class NetworkStatusMonitor(QObject):
     @classmethod
     def get_network_status_monitor(cls) -> 'NetworkStatusMonitor':
         if sys.platform == 'darwin':
@@ -22,9 +24,21 @@ class NetworkStatusMonitor:
             except (UnsupportedException, DBusException):
                 return NullNetworkStatusMonitor()
 
+    network_status_changed = pyqtSignal(bool, name="networkStatusChanged")
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
     def is_network_status_available(self):
         """Is the network status really available, and not just a dummy implementation?"""
         return type(self) is not NetworkStatusMonitor
+
+    def is_network_active(self) -> bool:
+        """Is there an active network connection.
+
+        True signals that the network is up. The internet may still not be reachable though.
+        """
+        raise NotImplementedError()
 
     def is_network_metered(self) -> bool:
         """Is the currently connected network a metered connection?"""
@@ -46,6 +60,12 @@ class SystemWifiInfo(NamedTuple):
 
 class NullNetworkStatusMonitor(NetworkStatusMonitor):
     """Dummy implementation, in case we don't have one for current platform."""
+
+    def __init__(self):
+        super().__init__()
+
+    def is_network_active(self):
+        return True
 
     def is_network_status_available(self):
         return False
