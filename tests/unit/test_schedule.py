78648b1a715f5b9841454e06b64abc59dc06bcb0
yeyomontana <139665002+yeyomontana@users.noreply.github.com>
2026-03-16 00:41:45 +0300

    Add Select All and Select None buttons to network selection UI

--- tests/unit/test_schedule.py
+++ tests/unit/test_schedule.py
@@ -8,7 +8,7 @@ from PyQt6.QtWidgets import QWidget
 
 import vorta.scheduler
 from vorta.application import VortaApp
-from vorta.store.models import BackupProfileModel, EventLogModel
+from vorta.store.models import BackupProfileModel, EventLogModel, WifiSettingModel
 from vorta.views.schedule_tab import ScheduleTab
 
 PROFILE_NAME = 'Default'
@@ -92,3 +92,41 @@ def test_schedule_tab_forwards_profile_provider_to_child_pages(qapp: VortaApp, q
     assert tab.shellCommandsPage.profile().id == profile.id
     assert tab.networksPage.profile().id == profile.id
     assert tab.logPage.profile().id == profile.id
+
+
+def test_networks_page_select_all_and_none_persist(qapp: VortaApp, qtbot):
+    profile = BackupProfileModel.get(name=PROFILE_NAME)
+    WifiSettingModel.create(ssid='Home WiFi', allowed=False, profile=profile.id)
+    WifiSettingModel.create(ssid='Office WiFi', allowed=True, profile=profile.id)
+
+    host = QWidget()
+    qtbot.addWidget(host)
+    tab = ScheduleTab(host, profile_provider=lambda: BackupProfileModel.get(id=profile.id))
+
+    tab.networksPage.populate_wifi()
+
+    qtbot.mouseClick(tab.networksPage.selectAllButton, QtCore.Qt.MouseButton.LeftButton)
+
+    assert [tab.networksPage.wifiListWidget.item(i).checkState() for i in range(tab.networksPage.wifiListWidget.count())] == [
+        QtCore.Qt.CheckState.Checked,
+        QtCore.Qt.CheckState.Checked,
+    ]
+    assert (
+        WifiSettingModel.select()
+        .where(WifiSettingModel.profile == profile.id, WifiSettingModel.allowed == True)  # noqa
+        .count()
+        == 2
+    )
+
+    qtbot.mouseClick(tab.networksPage.selectNoneButton, QtCore.Qt.MouseButton.LeftButton)
+
+    assert [tab.networksPage.wifiListWidget.item(i).checkState() for i in range(tab.networksPage.wifiListWidget.count())] == [
+        QtCore.Qt.CheckState.Unchecked,
+        QtCore.Qt.CheckState.Unchecked,
+    ]
+    assert (
+        WifiSettingModel.select()
+        .where(WifiSettingModel.profile == profile.id, WifiSettingModel.allowed == False)  # noqa
+        .count()
+        == 2
+    )
