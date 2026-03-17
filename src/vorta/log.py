0c25fdfb31222d67a1c43184bc45af1e7d509c10
Depavath Ratna <depavathratna2004@gmail.com>
2026-03-17 12:06:10 +0530

    Add type annotations to log module

--- src/vorta/log.py
+++ src/vorta/log.py
@@ -11,19 +11,28 @@ from logging.handlers import TimedRotatingFileHandler
 
 from vorta import config
 
-logger = logging.getLogger()
+logger: logging.Logger = logging.getLogger()
 
 
-def init_logger(background=False):
+def init_logger(background: bool = False) -> None:
+    """Initialize the application logger with file and optional console handlers.
+
+    Parameters
+    ----------
+    background : bool
+        If True, only log to file. If False, also log to console (stdout).
+    """
     logger.setLevel(logging.DEBUG)
     logging.getLogger('peewee').setLevel(logging.INFO)
     logging.getLogger('PyQt6').setLevel(logging.INFO)
 
     # create logging format
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter: logging.Formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     # create handlers
-    fh = TimedRotatingFileHandler(config.LOG_DIR / 'vorta.log', when='d', interval=1, backupCount=5)
+    fh: TimedRotatingFileHandler = TimedRotatingFileHandler(
+        config.LOG_DIR / 'vorta.log', when='d', interval=1, backupCount=5
+    )
     # ensure ".log" suffix
     fh.namer = lambda log_name: log_name.replace(".log", "") + ".log"
     fh.setLevel(logging.DEBUG)
@@ -33,7 +42,7 @@ def init_logger(background=False):
     if background:
         pass
     else:  # log to console, when running in foreground
-        ch = logging.StreamHandler()
+        ch: logging.StreamHandler = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)
         ch.setFormatter(formatter)
         logger.addHandler(ch)
