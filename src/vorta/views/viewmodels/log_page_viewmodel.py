17221b553321eb54480325f9f034451d3a74ca84
ebuzerdrmz44 <durmazs540@gmail.com>
2026-05-25 13:10:08 +0300

    fix make lint1

--- src/vorta/views/viewmodels/log_page_viewmodel.py
+++ src/vorta/views/viewmodels/log_page_viewmodel.py
@@ -1,7 +1,8 @@
 from vorta.store.models import EventLogModel
 
+
 class LogPageViewModel:
-    def get_event_logs(self,profile_id):
+    def get_event_logs(self, profile_id):
         """Fetch event logs for a specific profile, ordered from newest to oldest."""
         event_logs = [
             s
